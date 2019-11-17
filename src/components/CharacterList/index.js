757fcb34c8bd005ff2f0baaa61c0046735aac7c7
Vanessa Ating <atingenkay@gmail.com>
2019-11-18 00:16:09 +0100

    display characters from the API

--- src/components/CharacterList/index.js
+++ src/components/CharacterList/index.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 import Character from '../Character'
+
+const FEED_QUERY = gql`
+{
+  characters(page: 1) {
+    info {
+      count
+    }
+    results {
+      name,
+      status
+    }
+  }
+}
+`
 
 class CharacterList extends Component {
   render() {
-    const CharactersToRender = [
-      {
-        id: '1',
-        name: 'Rick',
-        status: 'Alive'
-      },
-      {
-        id: '2',
-        name: 'Morty',
-        status: 'Unknown'
-      },
-    ]
-
     return (
-      <div>{CharactersToRender.map(character => <Character key={character.id} character={character} />)}</div>
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+
+          const CharactersToRender = data.characters.results
+
+          return (
+            <div>
+              {CharactersToRender.map(character =>
+                <Character
+                  key={character.id}
+                  character={character}
+                />)}
+            </div>
+          )
+        }}
+      </Query>
     )
   }
 }
